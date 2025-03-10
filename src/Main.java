adb61f98740c314cf60245ed33c072704827aea2
mmingoo <aldrn0323@naver.com>
2025-03-10 15:43:54 +0900

    s3_2346 복습

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
+import java.io.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        //풍선의 갯수 받기
+        int N = Integer.parseInt(br.readLine());
+
+        // 위치 입력받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Deque<Balloon> q = new ArrayDeque<>();
+
+        int [] moveLocation = new int[N];
+        for(int i =0; i<N; i++){
+            moveLocation[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 큐에 1반을 제외한 풍선을 넣기
+        for(int i = 1; i<N; i++){
+            q.add(new Balloon(i+1,moveLocation[i]));
+        }
+
+        //첫번재 풍선 종이
+        int moveValue = moveLocation[0];
+        sb.append("1 ");
+
+
+        while(!q.isEmpty()){
+            if(moveValue > 0){
+                for(int i = 1; i< moveValue;  i++){
+                    q.add(q.poll());
+                }
+
+
+                Balloon next = q.poll();
+                moveValue = next.location;
+                sb.append(next.idx+" ");
+
+            }else{
+                for(int i = 1; i< -moveValue;  i++){
+                    q.addFirst(q.pollLast());
+                }
+                Balloon next = q.pollLast();
+                moveValue = next.location;
+                sb.append(next.idx+" ");
+
+            }
+        }
+
+        System.out.print(sb);
     }
 }
+
+class Balloon{
+    int idx;
+    int location;
+    public Balloon(int idx, int location){
+        this.idx = idx;
+        this.location = location;
+
+    }
+}
+
--- src/queue/s3_2346.java
+++ src/queue/s3_2346.java
@@ -1,70 +1,75 @@
 package queue;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class s3_2346 {
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        Deque<Balloon> queue = new ArrayDeque<>();
+        //풍선의 갯수 받기
+        int N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine()); //풍선의 개수
-
+        // 위치 입력받기
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] balloonLocation = new int[N];
-        for (int i = 0; i < N; i++) {
-            balloonLocation[i] = Integer.parseInt(st.nextToken());
+        Deque<Balloon> q = new ArrayDeque<>();
+
+        int [] moveLocation = new int[N];
+        for(int i =0; i<N; i++){
+            moveLocation[i] = Integer.parseInt(st.nextToken());
         }
 
-        br.close();
-        sb.append("1 "); //무조건 첫번째 풍선 먼저 터트리기 때문
-        int moveValue = balloonLocation[0]; //움직여야할 이동 값
-
-        //인덱스값과 인덱스가 가지고 있는 이동 값을 같이 넣어준다.
-        // i+1 : (처음 풍선은 넣어주지 않음) => 가장 처음 터트리기 때문
-        for(int i = 1; i < N; i++){
-            queue.add(new Balloon(i+1, balloonLocation[i]));
+        // 큐에 1반을 제외한 풍선을 넣기
+        for(int i = 1; i<N; i++){
+            q.add(new Balloon(i+1,moveLocation[i]));
         }
 
-        //해당 풍선이 모두 터질때 까지 반복
-        while(!queue.isEmpty()){
-            //양수일 경우 인덱스가 큰 쪽으로
+        //첫번재 풍선 종이
+        int moveValue = moveLocation[0];
+        sb.append("1 ");
+
+
+        while(!q.isEmpty()){
             if(moveValue > 0){
-                //앞에 있는 요소를 모두 뒤로 보낸다.
-                for(int i = 1; i < moveValue; i++){
-                    queue.add(queue.poll());
+                for(int i = 1; i< moveValue;  i++){
+                    q.add(q.poll());
                 }
-                //모두 뒤로 보냈다면
-                Balloon next = queue.poll();
-                moveValue = next.numValue; //이동값 갱신
-                sb.append(next.index+" "); //터트린 풍선의 위치값 출력 목록에 저장
-            }
-            //음수일 경우 인덱스가 작은 쪽으로
-            else{
-                //뒤에 있는 요소들 모두 앞으로 보낸다.
-                for(int i = 1; i < -moveValue; i++){
-                    queue.addFirst(queue.pollLast());
-                }
-                //모두 뒤로 보냈다면
-                Balloon next = queue.pollLast();
-                moveValue = next.numValue;
-                sb.append(next.index+" ");
-            }
 
+
+                Balloon next = q.poll();
+                moveValue = next.location;
+                sb.append(next.idx+" ");
+
+            }else{
+                for(int i = 1; i< -moveValue;  i++){
+                    q.addFirst(q.pollLast());
+                }
+                Balloon next = q.pollLast();
+                moveValue = next.location;
+                sb.append(next.idx+" ");
+
+            }
         }
-        System.out.println(sb);
-    }
-}
-//풍선의 인덱스와 숫자 값을 저장하는 클래스 Balloon
-class Balloon{
-    int index;
-    int numValue;
 
-    public Balloon(int index, int numValue) {
-        this.index = index;
-        this.numValue = numValue;
+        System.out.print(sb);
+    }
+
+}
+class Balloon{
+    int idx;
+    int location;
+    public Balloon(int idx, int location){
+        this.idx = idx;
+        this.location = location;
+
     }
 }
+
